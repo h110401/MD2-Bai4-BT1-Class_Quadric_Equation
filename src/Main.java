483ab325bcd29926e7c3161140b12fdd5bf7a53c
h110401 <hung110401@gmail.com>
2022-07-28 15:20:34 +0700

    bt1

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a, b, c:");
         double a = sc.nextDouble();
+        if (a == 0) {
+            System.out.println("a must not equals 0");
+            return;
+        }
         double b = sc.nextDouble();
         double c = sc.nextDouble();
 
-        if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                    System.out.println("The equation has infinity roots");
-                } else {
-                    System.out.println("The equation has no real roots");
-                }
-            } else {
-                System.out.printf("The equation has one root %.5f", -c / b);
-            }
+
+        QuadraticEquation q = new QuadraticEquation(a, b, c);
+
+        double delta = q.getDiscriminant();
+        if (delta > 0) {
+            System.out.printf("The equation has two roots %.5f and %.5f", q.getRoot1(), q.getRoot1());
+        } else if (delta == 0) {
+            System.out.printf("The equation has one root %.5f", q.getRoot1());
         } else {
-            QuadraticEquation q = new QuadraticEquation(a, b, c);
-
-            double delta = q.getDiscriminant();
-            if (delta > 0) {
-                System.out.printf("The equation has two roots %.5f and %.5f", q.getRoot1(), q.getRoot1());
-            } else if (delta == 0) {
-                System.out.printf("The equation has one root %.5f", q.getRoot1());
-            } else {
-                System.out.println("The equation has no real roots");
-            }
+            System.out.println("The equation has no real roots");
         }
-
     }
 }
